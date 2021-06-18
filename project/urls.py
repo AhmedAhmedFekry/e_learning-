0f14fd4d66ea5e388caea549ba0ff0426ab52dee
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-06-18 11:38:51 +0200

    make urls and template in chat app

--- project/urls.py
+++ project/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
       path('course/', include('courses.urls')),
     path('students/', include('students.urls')),
+    path('chat/', include('chat.urls', namespace='chat')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
