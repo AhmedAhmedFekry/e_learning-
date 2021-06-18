7c45febb0c49ed6d138c1d0b3d8ab1b1a503d9f2
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-06-18 17:30:36 +0200

    Implementing the WebSocket client

--- project/routing.py
+++ project/routing.py
@@ -1,16 +1,12 @@
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-# import chat.routing
-
-# application = ProtocolTypeRouter({
-#     'websocket': AuthMiddlewareStack(
-#         URLRouter(
-#             chat.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
+import chat.routing
 
 application = ProtocolTypeRouter({
- # empty for now
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
